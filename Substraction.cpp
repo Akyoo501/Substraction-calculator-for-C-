f8486ab72de2a7934e0abf11d3d685ff333e1020
Akyoo501 <143004721+Akyoo501@users.noreply.github.com>
2024-12-03 22:13:37 +0300

    Add files via upload

---
+++ Substraction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x;
+	int y;
+	
+	cout<<"Enter first number: ";
+	cin>>x;
+	cout<<"Enter second number: ";
+	cin>>y;
+	
+	cout<<"Result is: "<<x-y;
+	
+	return 0;
+}
